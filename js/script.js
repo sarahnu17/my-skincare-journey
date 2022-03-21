ab3f7320975c1a46c17355b73519dbd3323beb1c
Sarah Nuno <saritanu321@yahoo.com>
2022-03-21 12:34:30 -0700

    CloseModal JS code added to be able to close popup modals. Made closebutton visible on larger screens.

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const closeModalButtons = document.querySelectorAll('[data-close-button]');
 
 const overlay = document.getElementById('overlay');
 
-
+// To capture if user clicks on an item category on the item menu list
 openModalButtons.forEach( btn => {
     btn.addEventListener('click', () => {
         const modal = document.querySelectorAll(btn.dataset.target);
@@ -12,12 +12,29 @@ openModalButtons.forEach( btn => {
     })
 });
 
+// To capture if user clicks on an item preview to open up modal popup for the item
 openModalPopUp.forEach( btn => {
     btn.addEventListener('click', () => {
         const products = document.querySelectorAll(btn.dataset.modalTarget);
         openModalPop(products);
     })
 });
+
+// To capture if user clicks the background overlay to close the modal popup
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.products.active')
+    modals.forEach(products => {
+      closeModal(products)
+    })
+})
+
+// To capture if user clicks the close btn to close the modal popup
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const products = button.closest('.products')
+      closeModal(products)
+    })
+})
 
 
 function openModal(modal) {
@@ -43,5 +60,14 @@ function openModalPop(products) {
     }
     // Add the black overlay to background of the popup item
     overlay.classList.add('active');
+
+}
+
+function closeModal(products) {
+    if (products == null) return
+
+    // Remove the active classes from products and overlay when modal is closed
+    products.classList.remove('active');
+    overlay.classList.remove('active');
 
 }
