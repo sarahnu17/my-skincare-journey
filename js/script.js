6f5fef51ac634210d0f9990147ad421ce4c51bb4
Sarah Nuno <saritanu321@yahoo.com>
2022-03-23 14:14:05 -0700

    Fixed small bugs for google fonts and transitions. Default font was loading instead of desired font on page load. Transitions were showing up on page load.

--- js/script.js
+++ js/script.js
@@ -1,73 +1,81 @@
-const openModalButtons = document.querySelectorAll('[data-target]');
-const openModalPopUp = document.querySelectorAll('[data-modal-target]');
-const closeModalButtons = document.querySelectorAll('[data-close-button]');
+window.onload = function() {
+    document.body.classList.remove('preload');
 
-const overlay = document.getElementById('overlay');
+    const openModalButtons = document.querySelectorAll('[data-target]');
+    const openModalPopUp = document.querySelectorAll('[data-modal-target]');
+    const closeModalButtons = document.querySelectorAll('[data-close-button]');
 
-// To capture if user clicks on an item category on the item menu list
-openModalButtons.forEach( btn => {
-    btn.addEventListener('click', () => {
-        const modal = document.querySelectorAll(btn.dataset.target);
-        openModal(modal);
-    })
-});
+    const overlay = document.getElementById('overlay');
 
-// To capture if user clicks on an item preview to open up modal popup for the item
-openModalPopUp.forEach( btn => {
-    btn.addEventListener('click', () => {
-        const products = document.querySelectorAll(btn.dataset.modalTarget);
-        openModalPop(products);
-    })
-});
-
-// To capture if user clicks the background overlay to close the modal popup
-overlay.addEventListener('click', () => {
-    const modals = document.querySelectorAll('.products.active')
-    modals.forEach(products => {
-      closeModal(products)
-    })
-})
-
-// To capture if user clicks the close btn to close the modal popup
-closeModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-      const products = button.closest('.products')
-      closeModal(products)
-    })
-})
-
-
-function openModal(modal) {
-    if(modal == null) return
-
-    // Get rid of previous items shown on screen
-    document.querySelectorAll('.item').forEach(function(el) {
-        el.style.display = "none";
+    // To capture if user clicks on an item category on the item menu list
+    openModalButtons.forEach( btn => {
+        btn.addEventListener('click', () => {
+            const modal = document.querySelectorAll(btn.dataset.target);
+            openModal(modal);
+        })
     });
 
-    //Then displays only the targeted menu items
-    modal.forEach(function(el) {
-        el.style.display = "grid";
+    // To capture if user clicks on an item preview to open up modal popup for the item
+    openModalPopUp.forEach( btn => {
+        btn.addEventListener('click', () => {
+            const products = document.querySelectorAll(btn.dataset.modalTarget);
+            openModalPop(products);
+        })
     });
-}
 
-function openModalPop(products) {
-    if (products == null) return
+    // To capture if user clicks the background overlay to close the modal popup
+    overlay.addEventListener('click', () => {
+        const modals = document.querySelectorAll('.products.active')
+        modals.forEach(products => {
+        closeModal(products)
+        })
+    })
 
-    // Loop through the products array and add active class to the product that was clicked on
-    for(var i = 0; i < products.length; i++) {
-        products[i].classList.add('active');
+    // To capture if user clicks the close btn to close the modal popup
+    closeModalButtons.forEach(button => {
+        button.addEventListener('click', () => {
+        const products = button.closest('.products')
+        closeModal(products)
+        })
+    })
+
+
+    function openModal(modal) {
+        if(modal == null) return
+
+        // Get rid of previous items shown on screen
+        document.querySelectorAll('.item').forEach(function(el) {
+            el.style.display = "none";
+        });
+
+        //Then displays only the targeted menu items
+        modal.forEach(function(el) {
+            el.style.display = "grid";
+        });
     }
-    // Add the black overlay to background of the popup item
-    overlay.classList.add('active');
 
-}
+    function openModalPop(products) {
+        if (products == null) return
 
-function closeModal(products) {
-    if (products == null) return
+        // Loop through the products array and add active class to the product that was clicked on
+        for(var i = 0; i < products.length; i++) {
+            products[i].classList.add('active');
+        }
+        // Add the black overlay to background of the popup item
+        overlay.classList.add('active');
 
-    // Remove the active classes from products and overlay when modal is closed
-    products.classList.remove('active');
-    overlay.classList.remove('active');
+    }
 
-}
+    function closeModal(products) {
+        if (products == null) return
+
+        // Remove the active classes from products and overlay when modal is closed
+        products.classList.remove('active');
+        overlay.classList.remove('active');
+
+    }
+
+
+};
+
+
